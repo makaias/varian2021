0d24c16acc5638edc118113cfeeba532521f7e9f
makaias <sandorattilamakai@gmail.com>
2021-10-22 17:30:38 +0200

    created default implementation of doctor api, including the endpoints, controllers and services

--- backend/api/doctor/controllers/doctor.js
+++ backend/api/doctor/controllers/doctor.js
@@ -1,3 +1,123 @@
 'use strict';
 
-module.exports = {};
+module.exports = {
+    async getMe(ctx) {
+        return await strapi.services.doctor.getMe(ctx.state.user);
+    },
+
+    async createUser(ctx) {
+        if (ctx.state.user?.userType !== "DOCTOR") {
+            return null;
+        }
+        return await strapi.services.doctor.createUser(ctx.request.body);
+    },
+
+    /**
+     * example treatment plan in body:
+     * {
+     *     "treatmentPlan": [
+     *          "patientName": "name",
+     *          "phone": "phone",
+     *          "patientDOB": "asdf",
+     *          "email": "email",
+     *          "primaryCareProvider": "provider",
+     *          "surgeon": "surgeon",
+     *          "radiantOncologist": "name",
+     *          "medicalOncologist": "name",
+     *          "otherProviders" [
+     *                  "provider", "provider2"
+     *              ],
+     *          "cancerType": "type",
+     *          "DiagnosysDate": date,
+     *          "stage": "stage",
+     *          "surgery": true/false,
+     *          "surgeryDate": date/null" (ha null nem kell),
+     *          "SurgicalProcedure": "...",
+     *          "radiation": true/false,
+     *          "bodyAreaTreated": "area",
+     *          "endYear": 2021,
+     *          "systemicTherapy": true/false,
+     *          "agentsUsed": [
+     *                  "agent1", "agent2"
+     *              ],
+     *          "sympthomsSideEffects": true/false,
+     *          "symptomsSideEffectsDescription": "...",
+     *          "needOngoingTreatment": true/false,
+     *          "additionalTreatment": [
+     *                  "treatment1", "treatment2"
+     *              ],
+     *          "clinicVisits": [
+     *                  {
+     *                      "provider": "...",
+     *                      "when": date
+     *                  },
+     *                  {
+     *                      "provider": "...",
+     *                      "when": date
+     *                  }
+     *              ],
+     *          "survillenceTests": [
+     *                  {
+     *                      "provider": "...",
+     *                      "howOften": "2 times a week"
+     *                      "test": "..."
+     *                  },
+     *                  {
+     *                      "provider": "...",
+     *                      "howOften": "once a month"
+     *                      "test": "..."
+     *                  }
+     *              ],
+     *          "otherComments": "...",
+     *          "preparedBy": "name",
+     *          "deliveredOn": date
+     *      ]
+     * }
+     */
+    async createTreatmentPlan(ctx) {
+        if (ctx.state.user?.userType !== "DOCTOR") {
+            return null;
+        }
+        return await strapi.services.doctor.createTreatmentPlan(ctx.request.body);
+    },
+
+    /**
+     * Example json for document:
+     * {
+     *      "patient": 1
+     *      "content": "..."
+     * }
+     */
+    async createDocument(ctx) {
+        if (ctx.state.user?.userType !== "DOCTOR") {
+            return null;
+        }
+        return await strapi.services.doctor.createDocument(ctx.request.body);
+    },
+
+        /**
+     * Example json for document:
+     * {
+     *      "name": "name"
+     *      "elements": [
+     *          {
+     *              "type": "exampletype",
+     *              ...
+     *          }
+     *      ]
+     * }
+     */
+    async createSurveyTemplate(ctx) {
+        if (ctx.state.user?.userType !== "DOCTOR") {
+            return null;
+        }
+        return await strapi.services.doctor.createSurveyTemplate(ctx.request.body);
+    },
+
+    async patients(ctx) {
+        if (ctx.state.user?.userType !== "DOCTOR") {
+            return null;
+        }
+        return await strapi.services.doctor.patients(ctx.state.user.id);
+    }
+}
--- backend/api/doctor/services/doctor.js
+++ backend/api/doctor/services/doctor.js
@@ -1,3 +1,36 @@
 'use strict';
 
-module.exports = {};
+module.exports = {
+    async getMe(user) {
+        if (user){
+            return user.id;
+        }
+        return null;
+    },
+
+    //body based on patient.create
+    async createUser(body) {
+        let patientId = await strapi.services.patient.create(body);
+        return patientId;
+    },
+
+    async createTreatmentPlan(body) {
+        let planId = await strapi.services["treatment-plan"].create(body);
+        return planId;
+    },
+
+    async createDocument(body) {
+        let documentId = await strapi.services.document.create(body);
+        return documentId;
+    },
+
+    async createSurveyTemplate(body) {
+        let surveyTemplateId = await strapi.services["survey-template"].create(body);
+        return surveyTemplateId;
+    },
+
+    async patients(id) {
+        let patientIds = await strapi.services.patient.findByDoctor(user.id);
+        return patientIds;
+    }
+};
