fe3c94453174d8ef689229174c745736b7ba27ee
Dzsani <gatsjanos@gmail.com>
2021-10-22 22:51:29 +0200

    Formatting

--- frontend/src/app/routing.tsx
+++ frontend/src/app/routing.tsx
@@ -1,31 +1,31 @@
-import React, {ReactElement} from "react";
-import {Route, Switch} from "react-router-dom";
-import Home from "../pages/Home";
-import {useAuthBackend} from "../context/AuthBackend";
-import LoginWall from "../components/login/LoginWall";
+import React, {ReactElement} from 'react';
+import {Route, Switch} from 'react-router-dom';
+import Home from '../pages/Home';
+import {useAuthBackend} from '../context/AuthBackend';
+import LoginWall from '../components/login/LoginWall';
 
 interface Props {
 }
 
 export default function Routing({}: Props): ReactElement {
-    const authBackend = useAuthBackend();
+  const authBackend = useAuthBackend();
 
-    const routesRequiringLogin = [
-        <Route exact path="/me" component={() => <p>User profile page</p>}/>,
-        <Route exact path="/exampleNeedsLogin" component={() => <p>exampleNeedsLogin</p>}/>
-    ];
-    return (
-        <Switch>
-            <Route exact path="/" component={Home}/>
+  const routesRequiringLogin = [
+    <Route exact path='/me' component={() => <p>User profile page</p>} />,
+    <Route exact path='/exampleNeedsLogin' component={() => <p>exampleNeedsLogin</p>} />
+  ];
+  return (
+    <Switch>
+      <Route exact path='/' component={Home} />
 
-            {authBackend.isLoggedIn && routesRequiringLogin}
+      {authBackend.isLoggedIn && routesRequiringLogin}
 
-            <Route exact path="/articles" component={() => <p>Articles page</p>}/>
+      <Route exact path='/articles' component={() => <p>Articles page</p>} />
 
-            {!authBackend.isLoggedIn && (
-                <LoginWall/>
-            )}
-            <Route component={() => <p>Not found</p>}/>
-        </Switch>
-    );
+      {!authBackend.isLoggedIn && (
+        <LoginWall />
+      )}
+      <Route component={() => <p>Not found</p>} />
+    </Switch>
+  );
 }
