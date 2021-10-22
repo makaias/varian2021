e06cf7413e7484e66e5708617cc1e168205859b0
makaias <sandorattilamakai@gmail.com>
2021-10-22 12:01:56 +0200

    created default strapi backend #1

---
+++ backend/varian_2021_backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c2f12b0e8215124a28a2048ea6d602b5'),
+    },
+  },
+});
---
+++ backend/varian_2021_backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '59fb31ea-5b63-4399-9e56-538fa9e10a01'
+};
