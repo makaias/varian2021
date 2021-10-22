1fe05e2cf1d4ccb36b75147caf39c6d37439899c
Szalay Szabolcs <szotyi990204@gmail.com>
2021-10-22 22:44:23 +0200

    working: doctor->create-user, patient->auth,  fill-data, dependency updates

--- backend/api/patient/services/patient.js
+++ backend/api/patient/services/patient.js
@@ -1,8 +1,44 @@
 "use strict";
+const { Boom } = require("boom");
+
+function isPersonalDataComplete(user, newData) {
+  const dataFieldsToValidate = [
+    "username",
+    "email",
+    "password",
+    "firstname",
+    "surename",
+    "date_of_birth",
+  ];
+  for (const dataFieldToValidate of dataFieldsToValidate) {
+    if (!user[dataFieldToValidate] && !newData[dataFieldToValidate]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+async function updateUser(id, data) {
+  const entity = await strapi
+    .query("user", "users-permissions")
+    .update(id, data);
+  return entity;
+}
 
 module.exports = {
-    async findByDoctor(doctorId) {
-        let doctor = await strapi.services.user.findOne({id:doctorId}, ["patients"]);
-        return doctor?.patients?.map(p => p.id);
+  async update(user, data) {
+    data["complete"] = isPersonalDataComplete(user, data);
+    // console.log(data);
+    if (data["complete"]) {
+      return await updateUser({ id: user.id }, data);
     }
+    throw Boom.notAcceptable("must fill all datafields", data);
+  },
+
+  async getPatients(doctorId) {
+    let patients = await strapi.plugins[
+      "users-permissions"
+    ].services.user.fetch({ doctor: doctorId });
+    return patients;
+  },
 };
