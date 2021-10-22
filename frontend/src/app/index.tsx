08d8da56a81059a45de678491bb88488714e5442
Bendegúz Cseresnyés <cser.bende@gmail.com>
2021-10-22 17:56:41 +0200

    Start implementing layout

---
+++ frontend/src/app/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Chakra from "./chakra";
+import Layout from "./layout";
+import "./style.css";
+
+function App() {
+  return (
+    <Chakra>
+      <Layout></Layout>
+    </Chakra>
+  );
+}
+
+export default App;
