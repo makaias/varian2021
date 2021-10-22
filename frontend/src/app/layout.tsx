99b638a6a1a7bd604a9c38cbf9a35d28984f7082
Bendegúz Cseresnyés <cser.bende@gmail.com>
2021-10-22 18:20:13 +0200

    Add logo to layout

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,19 +1,24 @@
-import { Box, Container, Text, VStack } from "@chakra-ui/layout";
-import { HStack } from "@chakra-ui/react";
+import { Box, Container, HStack, Image, VStack } from "@chakra-ui/react";
 import React, { PropsWithChildren, ReactElement } from "react";
+import logo from "./logo.svg";
 
-export default function Layout({}: PropsWithChildren<{}>): ReactElement {
+export default function Layout({
+  children,
+}: PropsWithChildren<{}>): ReactElement {
   const { user } = { user: null };
 
   return (
     <VStack align="stretch" bg="secondary.100">
-      <Box bg="white" p={4}>
+      <Box bg="white" p={3}>
         <Container maxWidth="container.lg">
-          <HStack justify="center">
-            <Text fontSize="3xl">Varian</Text>
-          </HStack>
+          {!user && (
+            <HStack justify="center">
+              <Image height="5rem" src={logo} />
+            </HStack>
+          )}
         </Container>
       </Box>
+      <Container maxWidth="container.lg">{children}</Container>
     </VStack>
   );
 }
