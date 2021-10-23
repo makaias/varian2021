41a130ab242b5c78797b101efdfcacaf27d2f0a3
Szalay Szabolcs <szotyi990204@gmail.com>
2021-10-23 20:56:36 +0200

    Update endpoint names, add order document by date

--- backend/api/document/controllers/document.js
+++ backend/api/document/controllers/document.js
@@ -9,6 +9,15 @@ module.exports = {
   async getMe(ctx) {
     return await strapi.services.document.find({
       patient: ctx.state.user?.id,
+      _sort: "created:DESC",
     });
+  },
+
+  async create(ctx) {
+    let entity;
+    const body = ctx.request.body;
+    const now = Date.now();
+    entity = await strapi.services.document.create({ ...body, created: now });
+    return entity;
   },
 };
