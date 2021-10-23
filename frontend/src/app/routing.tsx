5eff9322f62328613e78e4bca2a2e25260ff34c3
Dzsani <gatsjanos@gmail.com>
2021-10-23 03:31:50 +0200

    Added Article listing and reading pages

--- frontend/src/app/routing.tsx
+++ frontend/src/app/routing.tsx
@@ -1,19 +1,25 @@
-import React, {ReactElement} from "react";
-import {Route, Switch} from "react-router-dom";
-import Home from "../pages/Home";
-import {useAuthBackend} from "../context/AuthBackend";
-import LoginWall from "../components/login/LoginWall";
-import AtiGrafikontTesztel from "../pages/AtiGrafikontTesztel";
+import React, {ReactElement} from 'react';
+import {Route, Switch, useParams} from 'react-router-dom';
+import Home from '../pages/Home';
+import {useAuthBackend} from '../context/AuthBackend';
+import LoginWall from '../components/login/LoginWall';
+import AtiGrafikontTesztel from '../pages/AtiGrafikontTesztel';
+import OneArticle from '../pages/article/OneArticle';
+import Articles from '../pages/article/Articles';
 
 const routesNotRequiringLogin = [
   <Route exact path='/' component={Home} />,
-  <Route exact path='/articles' component={() => <p>Articles page</p>} />
+  <Route exact path='/articles' component={Articles} />,
+  <Route exact path='/articles/:id' component={() => {
+    const {id} = useParams<{id}>();
+    return (<OneArticle id={id} />);
+  }} />
 ];
 
 const routesRequiringLogin = [
-    <Route exact path="/me" component={() => <p>User profile page</p>}/>,
-    <Route exact path="/exampleNeedsLogin" component={() => <p>exampleNeedsLogin</p>}/>,
-    <Route exact path="/ati" component={AtiGrafikontTesztel} />
+  <Route exact path='/me' component={() => <p>User profile page</p>} />,
+  <Route exact path='/exampleNeedsLogin' component={() => <p>exampleNeedsLogin</p>} />,
+  <Route exact path='/ati' component={AtiGrafikontTesztel} />
 ];
 
 export default function Routing(): ReactElement {
@@ -21,15 +27,15 @@ export default function Routing(): ReactElement {
 
   return (
     <Switch>
-        <Route exact path="/" component={Home}/>
+      <Route exact path='/' component={Home} />
 
-        {authBackend.isLoggedIn && routesRequiringLogin}
-        {routesNotRequiringLogin}
+      {authBackend.isLoggedIn && routesRequiringLogin}
+      {routesNotRequiringLogin}
 
-        {!authBackend.isLoggedIn && (
-          <LoginWall />
-        )}
-        <Route component={() => <p>Not found</p>} />
+      {!authBackend.isLoggedIn && (
+        <LoginWall />
+      )}
+      <Route component={() => <p>Not found</p>} />
     </Switch>
   );
 }
