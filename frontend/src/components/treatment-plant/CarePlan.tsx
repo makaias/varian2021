af8d8a4ee1ed7260e1347cfd4b27c347dc0436f6
Szalay Szabolcs <szotyi990204@gmail.com>
2021-10-23 21:49:32 +0200

    Add CarePlan for Bende

--- frontend/src/components/treatment-plant/CarePlan.tsx
+++ frontend/src/components/treatment-plant/CarePlan.tsx
@@ -1,7 +1,76 @@
-import React, {ReactElement} from 'react';
+import {ReactElement} from 'react';
+import UniformGrid from '../UniformGrid';
+import {GridContent, GridField} from './Fields';
 
 interface Props {}
 
 export default function CarePlan({}: Props): ReactElement {
-  return <div></div>;
+  return (
+    <UniformGrid column={15} gap={0}>
+      <GridContent color="primary.400" fontWeight="bold" fontSize="x1" colSpan={15}>
+        Follow-up Care Plan
+      </GridContent>
+
+      <GridContent fontWeight="bold" fontSize="x1" colSpan={15}>
+        Schedule of Clinical Visits
+      </GridContent>
+
+      <GridContent fontSize="x1" colSpan={6}>
+        Coordinating Provider
+      </GridContent>
+
+      <GridContent fontSize="x1" colSpan={9}>
+        When/How often
+      </GridContent>
+
+      <GridField name="ph" colSpan={6}></GridField>
+      <GridField name="ph" colSpan={9}></GridField>
+      <GridField name="ph" colSpan={6}></GridField>
+      <GridField name="ph" colSpan={9}></GridField>
+      <GridField name="ph" colSpan={6}></GridField>
+      <GridField name="ph" colSpan={9}></GridField>
+      <GridField name="ph" colSpan={6}></GridField>
+      <GridField name="ph" colSpan={9}></GridField>
+
+      <GridContent fontWeight="bold" fontSize="x1" colSpan={15}>
+        Cancer Surveillance or other Recommended Tests
+      </GridContent>
+
+      <GridContent fontSize="x1" colSpan={5}>
+        Coordinating Provider
+      </GridContent>
+
+      <GridContent fontSize="x1" colSpan={5}>
+        Test
+      </GridContent>
+
+      <GridContent fontSize="x1" colSpan={5}>
+        How Often
+      </GridContent>
+
+      <GridField name="ph" colSpan={5}></GridField>
+      <GridField name="ph" colSpan={5}></GridField>
+      <GridField name="ph" colSpan={5}></GridField>
+
+      <GridField name="ph" colSpan={5}></GridField>
+      <GridField name="ph" colSpan={5}></GridField>
+      <GridField name="ph" colSpan={5}></GridField>
+
+      <GridField name="ph" colSpan={5}></GridField>
+      <GridField name="ph" colSpan={5}></GridField>
+      <GridField name="ph" colSpan={5}></GridField>
+
+      <GridField name="ph" colSpan={5}></GridField>
+      <GridField name="ph" colSpan={5}></GridField>
+      <GridField name="ph" colSpan={5}></GridField>
+
+      <GridField name="ph" colSpan={5}></GridField>
+      <GridField name="ph" colSpan={5}></GridField>
+      <GridField name="ph" colSpan={5}></GridField>
+
+      <GridContent fontSize="x1" colSpan={15}>
+        TODO Bende csinád meeeg
+      </GridContent>
+    </UniformGrid>
+  );
 }
--- frontend/src/pages/TreatmentPlan.tsx
+++ frontend/src/pages/TreatmentPlan.tsx
@@ -1,13 +1,17 @@
 import {Formik} from 'formik';
 import React, {ReactElement} from 'react';
-import {General} from '../components/treatment-plant';
+import {CarePlan, General, Summary} from '../components/treatment-plant';
 
 interface Props {}
 
 export default function TreatmentPlan({}: Props): ReactElement {
   return (
     <Formik initialValues={{patientName: 'asd'}} onSubmit={() => {}}>
-      <General />
+      <>
+        <General />
+        <CarePlan />
+        <Summary />
+      </>
     </Formik>
   );
 }
