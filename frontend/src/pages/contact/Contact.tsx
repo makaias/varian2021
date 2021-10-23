f775dac841b4a6c3c777271b74d4a5d088ff4681
Dzsani <gatsjanos@gmail.com>
2021-10-23 20:00:59 +0200

    Contact page content added

--- frontend/src/pages/contact/Contact.tsx
+++ frontend/src/pages/contact/Contact.tsx
@@ -4,21 +4,27 @@ import {Image} from '@chakra-ui/react';
 import ContactPageImage from './contact_page_image.bmp';
 import {FaEnvelope, FaPhone} from 'react-icons/all';
 import UniformGrid from '../../components/UniformGrid';
+import {useLayoutConfig} from '../../app/layout';
 
 export default function Contact(): ReactElement {
+  useLayoutConfig({title: 'Contact', bg: 'plain'});
+
   return (
     <>
-      <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-        dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-        nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim
-        id est laborum.</Text>
+      <br />
+      <Text>
+        When you need treatment for cancer, you have a lot to learn and think about.
+        It is normal to feel overwhelmed and confused.
+        But, talking with your doctor and learning all you can about all your treatment options, including clinical
+        trials,
+        can help you make a decision you feel good about.
+      </Text>
 
       <VStack>
         <br />
         <br />
-        <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod te</Text>
-        <Text fontWeight={'bold'}> Lorem ipsum dolor sit amet, consectetur </Text>
+        <Text>If you or someone you care about has been diagnosed with cancer, we're here to help.</Text>
+        <Text fontWeight={'bold'}> Do not hesitate to contact us! </Text>
       </VStack>
 
       <UniformGrid columns={[1, 1, 2]}>
