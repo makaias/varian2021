6815c8ba44c4bcd5a9ea800483e3f19e462e7ecd
Bendegúz Cseresnyés <cser.bende@gmail.com>
2021-10-23 06:09:10 +0200

    Implement layout magic hook

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,14 +1,42 @@
-import { Box, Container, HStack, Image, VStack } from "@chakra-ui/react";
-import React, { PropsWithChildren, ReactElement } from "react";
-import logo from "./logo.svg";
+import {Box, Container, Flex, HStack, Image, Text, VStack} from '@chakra-ui/react';
+import {faSignOutAlt, faUserCircle} from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import React, {PropsWithChildren, ReactElement, useContext, useEffect, useState} from 'react';
+import {useAuthBackend} from '../context/AuthBackend';
+import logo from './logo.svg';
 
-export default function Layout({
-  children,
-}: PropsWithChildren<{}>): ReactElement {
-  const { user } = { user: null };
+export interface LayoutConfig {
+  title?: string;
+  bg: 'plain' | 'fancy';
+}
+
+const LayoutContext = React.createContext<(config: LayoutConfig) => void | null>(null);
+
+export function useLayoutConfig(config: LayoutConfig) {
+  const callback = useContext(LayoutContext);
+  useEffect(() => {
+    callback(config);
+    return () => callback(null);
+  }, [config]);
+}
+
+const defaultLayoutConfig: LayoutConfig = {bg: 'plain'};
+
+export default function Layout({children}: PropsWithChildren<{}>): ReactElement {
+  const {user} = useAuthBackend();
+  const [_layoutConfig, setLayoutConfig] = useState(null);
+  const layoutConfig = _layoutConfig || defaultLayoutConfig;
+
+  useEffect(() => {
+    document.getElementsByTagName('HTML')[0].classList.add(`layout-${layoutConfig.bg}`);
+
+    return () => {
+      document.getElementsByTagName('HTML')[0].classList.remove(`layout-${layoutConfig.bg}`);
+    };
+  }, [layoutConfig]);
 
   return (
-    <VStack align="stretch" bg="primary.50">
+    <VStack align="stretch">
       <Box bg="white" p={3}>
         <Container maxWidth="container.lg">
           {!user && (
@@ -16,10 +44,35 @@ export default function Layout({
               <Image height="5rem" src={logo} />
             </HStack>
           )}
+          {user && (
+            <HStack p={2} align="center" fontSize="xl" justify="space-between">
+              <HStack align="flex-end">
+                <Flex fontSize="4xl" color="primary.500">
+                  <FontAwesomeIcon icon={faUserCircle}></FontAwesomeIcon>
+                </Flex>
+                <Text fontWeight="bold">Profile</Text>
+              </HStack>
+              <Text>Results</Text>
+              <Text>Surveys</Text>
+              <Text>Symptoms</Text>
+              <Text>Publications</Text>
+              <Text fontWeight="bold">Contact</Text>
+              <Flex fontSize="4xl" color="primary.500">
+                <FontAwesomeIcon icon={faSignOutAlt}></FontAwesomeIcon>
+              </Flex>
+            </HStack>
+          )}
         </Container>
       </Box>
+      {layoutConfig.title && (
+        <Box bg="primary.400" color="white" p={1} textAlign="center" fontSize="2xl" fontWeight="bold">
+          {layoutConfig.title}
+        </Box>
+      )}
       <Box>
-        <Container maxWidth="container.lg">{children}</Container>
+        <Container maxWidth="container.lg">
+          <LayoutContext.Provider value={setLayoutConfig}>{children}</LayoutContext.Provider>
+        </Container>
       </Box>
     </VStack>
   );
--- frontend/src/pages/User.tsx
+++ frontend/src/pages/User.tsx
@@ -1,9 +1,17 @@
 import React, {ReactElement} from 'react';
+import {LayoutConfig, useLayoutConfig} from '../app/layout';
 import {useAuthBackend} from '../context/AuthBackend';
 
 interface Props {}
 
+const layoutConfig: LayoutConfig = {
+  bg: 'plain',
+  title: 'User page',
+};
+
 export default function User({}: Props): ReactElement {
+  useLayoutConfig(layoutConfig);
+
   const {user} = useAuthBackend();
 
   return <pre>{JSON.stringify(user, null, 2)}</pre>;
