1afe3d44e49ee6bb7710670ddd04cb70b64445ef
Szalay Szabolcs <szotyi990204@gmail.com>
2021-10-23 19:23:56 +0200

    Mittudomén, Dzsáni kérte

--- backend/api/document/controllers/document.js
+++ backend/api/document/controllers/document.js
@@ -1,8 +1,14 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async getMe(ctx) {
+    return await strapi.services.document.find({
+      patient: ctx.state.user?.id,
+    });
+  },
+};
