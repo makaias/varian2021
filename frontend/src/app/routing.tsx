801cfb969880cfa24ad53db33bb7af2bed955878
makaias <sandorattilamakai@gmail.com>
2021-10-23 05:25:33 +0200

    implemented dashboard with badges and linediagrams

--- frontend/src/app/routing.tsx
+++ frontend/src/app/routing.tsx
@@ -1,19 +1,16 @@
-import React, {ReactElement} from "react";
-import {Route, Switch} from "react-router-dom";
-import Home from "../pages/Home";
-import {useAuthBackend} from "../context/AuthBackend";
-import LoginWall from "../components/login/LoginWall";
-import AtiGrafikontTesztel from "../pages/AtiGrafikontTesztel";
+import React, {ReactElement} from 'react';
+import {Route, Switch} from 'react-router-dom';
+import Home from '../pages/Home';
+import {useAuthBackend} from '../context/AuthBackend';
+import LoginWall from '../components/login/LoginWall';
+import AtiGrafikontTesztel from '../pages/dashboard';
 
-const routesNotRequiringLogin = [
-  <Route exact path='/' component={Home} />,
-  <Route exact path='/articles' component={() => <p>Articles page</p>} />
-];
+const routesNotRequiringLogin = [<Route exact path="/" component={Home} />, <Route exact path="/articles" component={() => <p>Articles page</p>} />];
 
 const routesRequiringLogin = [
-    <Route exact path="/me" component={() => <p>User profile page</p>}/>,
-    <Route exact path="/exampleNeedsLogin" component={() => <p>exampleNeedsLogin</p>}/>,
-    <Route exact path="/ati" component={AtiGrafikontTesztel} />
+  <Route exact path="/me" component={() => <p>User profile page</p>} />,
+  <Route exact path="/exampleNeedsLogin" component={() => <p>exampleNeedsLogin</p>} />,
+  <Route exact path="/dashboard" component={AtiGrafikontTesztel} />,
 ];
 
 export default function Routing(): ReactElement {
@@ -21,15 +18,13 @@ export default function Routing(): ReactElement {
 
   return (
     <Switch>
-        <Route exact path="/" component={Home}/>
+      <Route exact path="/" component={Home} />
 
-        {authBackend.isLoggedIn && routesRequiringLogin}
-        {routesNotRequiringLogin}
+      {authBackend.isLoggedIn && routesRequiringLogin}
+      {routesNotRequiringLogin}
 
-        {!authBackend.isLoggedIn && (
-          <LoginWall />
-        )}
-        <Route component={() => <p>Not found</p>} />
+      {!authBackend.isLoggedIn && <LoginWall />}
+      <Route component={() => <p>Not found</p>} />
     </Switch>
   );
 }
---
+++ frontend/src/pages/dashboard/index.tsx
@@ -0,0 +1,91 @@
+import {VStack} from '@chakra-ui/layout';
+import {HStack, Image} from '@chakra-ui/react';
+import React from 'react';
+import LineDiagram from '../../components/diagrams/LineDiagram';
+import UniformGrid from '../../components/UniformGrid';
+import useEndpoint from '../../hooks/useEndpoint';
+
+import startLogo from './started.svg';
+import endLogo from './finished.svg';
+import firstMonthLogo from './firstmonth.svg';
+import secondMonthLogo from './secondmonth.svg';
+import thirdMonthLogo from './thirdmonth.svg';
+import {Interface} from 'readline';
+import {Statistic} from '../../model/Statistic';
+
+interface Props {}
+
+export default function Dashboard({}: Props) {
+  const patientId = 8;
+  const usedEndpoint = useEndpoint<Statistic>({
+    conf: {
+      url: `/statistics/${patientId}`,
+    },
+    deps: [patientId],
+  });
+
+  return (
+    <>
+      {usedEndpoint.pending && <p>Pending...</p>}
+      {usedEndpoint.failed && <p>Failed</p>}
+      {usedEndpoint.succeeded && (
+        <VStack align="stretch">
+          <HStack align="flex-start" justify="center">
+            {usedEndpoint.data.badges.map((badge) => {
+              switch (badge.type) {
+                case 'STARTED':
+                  return <Image w="5rem" src={startLogo} />;
+                case 'FINISHED':
+                  return <Image w="5rem" src={endLogo} />;
+                case 'ONE_MONTH':
+                  return <Image w="5rem" src={firstMonthLogo} />;
+                case 'TWO_MONTH':
+                  return <Image w="5rem" src={secondMonthLogo} />;
+                case 'THREE_MONTH':
+                  return <Image w="5rem" src={thirdMonthLogo} />;
+                default:
+              }
+            })}
+          </HStack>
+          <UniformGrid columns={[1, 1, 2]} gap={4}>
+            <LineDiagram
+              data={usedEndpoint.data.lifeStatisfaction.map((i) => i.value)}
+              labels={Array(usedEndpoint.data.lifeStatisfaction.length)
+                .fill(0)
+                .map((_, i) => i.toString())}
+              title="Life statisfaction"
+            />
+            <LineDiagram
+              data={usedEndpoint.data.eatingBehavior.map((i) => i.value)}
+              labels={Array(usedEndpoint.data.eatingBehavior.length)
+                .fill(0)
+                .map((_, i) => i.toString())}
+              title="Eating behavior"
+            />
+            <LineDiagram
+              data={usedEndpoint.data.sleeping.map((i) => i.value)}
+              labels={Array(usedEndpoint.data.sleeping.length)
+                .fill(0)
+                .map((_, i) => i.toString())}
+              title="Sleeping"
+            />
+            <LineDiagram
+              data={usedEndpoint.data.socialInteraction.map((i) => i.value)}
+              labels={Array(usedEndpoint.data.socialInteraction.length)
+                .fill(0)
+                .map((_, i) => i.toString())}
+              title="Social Interaction"
+            />
+            <LineDiagram
+              data={usedEndpoint.data.physicalActivity.map((i) => i.value)}
+              labels={Array(usedEndpoint.data.physicalActivity.length)
+                .fill(0)
+                .map((_, i) => i.toString())}
+              title="Physical Activity"
+            />
+          </UniformGrid>
+        </VStack>
+      )}
+    </>
+  );
+}
