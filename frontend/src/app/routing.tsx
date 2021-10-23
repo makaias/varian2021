2d370779188ef137e33fe2ab574bba9c9e5f013e
makaias <sandorattilamakai@gmail.com>
2021-10-23 06:10:33 +0200

    created basic symtoms page, made minor refactors in routing

--- frontend/src/app/routing.tsx
+++ frontend/src/app/routing.tsx
@@ -4,13 +4,16 @@ import Home from '../pages/Home';
 import {useAuthBackend} from '../context/AuthBackend';
 import LoginWall from '../components/login/LoginWall';
 import AtiGrafikontTesztel from '../pages/dashboard';
+import Dashboard from '../pages/dashboard';
+import Symptoms from '../pages/Symptoms';
 
 const routesNotRequiringLogin = [<Route exact path="/" component={Home} />, <Route exact path="/articles" component={() => <p>Articles page</p>} />];
 
 const routesRequiringLogin = [
   <Route exact path="/me" component={() => <p>User profile page</p>} />,
   <Route exact path="/exampleNeedsLogin" component={() => <p>exampleNeedsLogin</p>} />,
-  <Route exact path="/dashboard" component={AtiGrafikontTesztel} />,
+  <Route exact path="/dashboard" component={Dashboard} />,
+  <Route exact path="/symptoms" component={Symptoms} />,
 ];
 
 export default function Routing(): ReactElement {
