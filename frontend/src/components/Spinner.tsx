519c79661f34f21eadbbd006b012daf079d0a561
makaias <sandorattilamakai@gmail.com>
2021-10-23 16:54:29 +0200

    upgraded spinner and used it in the dashboard

--- frontend/src/components/Spinner.tsx
+++ frontend/src/components/Spinner.tsx
@@ -1,9 +1,8 @@
 import React, {FC} from 'react';
+import {Spinner as ChakraSpinner} from '@chakra-ui/react';
 
 const Spinner: FC = () => {
-  return (
-    <p>Pending...</p>
-  );
+  return <ChakraSpinner thickness="4px" speed="0.65s" emptyColor="gray.200" color="primary.500" size="xl" />;
 };
 
 export default Spinner;
