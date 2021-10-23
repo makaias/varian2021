6e176089b71163852c52ce7ea6d30a918c8f09b8
Bendegúz Cseresnyés <cser.bende@gmail.com>
2021-10-23 23:40:39 +0200

    Fix userSelector

--- frontend/src/components/selector/UserSelector.tsx
+++ frontend/src/components/selector/UserSelector.tsx
@@ -1,20 +1,28 @@
-import React, {FC} from 'react';
 import {Select} from '@chakra-ui/react';
+import React, {FC} from 'react';
 import {User} from '../../context/AuthBackend';
 
 interface Props {
   options: User[];
   userId: number;
-  setUserId: (newUserId: number) => void,
+  setUserId?: (newUserId: number) => void;
+  onChange?: React.ChangeEventHandler<HTMLSelectElement>;
 }
 
 const UserSelector: FC<Props> = (props) => {
-
   return (
-    <Select placeholder='Select user' value={props.userId}
-            onChange={e => props.setUserId(Number.parseInt(e.target.value))}>
-      {props.options.map(user => (
-        <option key={user.id} value={user.id}>{user.firstname + ' ' + user.surename}</option>
+    <Select
+      placeholder="Select user"
+      value={props.userId}
+      onChange={(e) => {
+        if (props.onChange) props.onChange(e);
+        if (props.setUserId) props.setUserId(Number.parseInt(e.target.value));
+      }}
+    >
+      {props.options.map((user) => (
+        <option key={user.id} value={user.id}>
+          {user.firstname + ' ' + user.surename}
+        </option>
       ))}
     </Select>
   );
