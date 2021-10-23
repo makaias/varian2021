e03aef77723d6bb22c7ff3d2d1486794b16b329a
Bendegúz Cseresnyés <cser.bende@gmail.com>
2021-10-23 14:38:19 +0200

    Fix Navbar

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import {Box, Container, Flex, HStack, Image, Text, VStack} from '@chakra-ui/react';
-import {faSignOutAlt, faUserCircle} from '@fortawesome/free-solid-svg-icons';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import React, {PropsWithChildren, ReactElement, useContext, useEffect, useState} from 'react';
+import React, {PropsWithChildren, ReactElement, ReactNode, useContext, useEffect, useState} from 'react';
+import {Link, useLocation} from 'react-router-dom';
+import {ExitIcon, UserIcon} from '../components/icons';
 import {useAuthBackend} from '../context/AuthBackend';
 import logo from './logo.svg';
 
@@ -25,7 +25,7 @@ export function useLayoutConfig(config: LayoutConfig) {
 const defaultLayoutConfig: LayoutConfig = {bg: 'plain'};
 
 export default function Layout({children}: PropsWithChildren<{}>): ReactElement {
-  const {user} = useAuthBackend();
+  const {user, logout} = useAuthBackend();
   const [_layoutConfig, setLayoutConfig] = useState(null);
   const layoutConfig = _layoutConfig || defaultLayoutConfig;
 
@@ -47,21 +47,22 @@ export default function Layout({children}: PropsWithChildren<{}>): ReactElement 
             </HStack>
           )}
           {user && (
-            <HStack p={2} align="center" fontSize="xl" justify="space-between">
-              <HStack align="flex-end">
-                <Flex fontSize="4xl" color="primary.500">
-                  <FontAwesomeIcon icon={faUserCircle}></FontAwesomeIcon>
-                </Flex>
-                <Text fontWeight="bold">Profile</Text>
-              </HStack>
-              <Text>Results</Text>
-              <Text>Surveys</Text>
-              <Text>Symptoms</Text>
-              <Text>Publications</Text>
-              <Text fontWeight="bold">Contact</Text>
-              <Flex fontSize="4xl" color="primary.500">
-                <FontAwesomeIcon icon={faSignOutAlt}></FontAwesomeIcon>
-              </Flex>
+            <HStack align="stretch" fontSize="xl" justify="space-between">
+              <NavItem to="/dashboard">
+                <HStack align="center">
+                  <UserIcon width="2em" />
+                  <Text>Profile</Text>
+                </HStack>
+              </NavItem>
+
+              <NavItem to="/results">Results</NavItem>
+              <NavItem to="/survey">Surveys</NavItem>
+              <NavItem to="/symptoms">Symptoms</NavItem>
+              <NavItem to="/publications">Publications</NavItem>
+              <NavItem to="/contact">Contact</NavItem>
+              <NavItem onClick={() => logout()}>
+                <ExitIcon width="2em" />
+              </NavItem>
             </HStack>
           )}
         </Container>
@@ -77,5 +78,31 @@ export default function Layout({children}: PropsWithChildren<{}>): ReactElement 
         </Container>
       </Box>
     </VStack>
+  );
+}
+
+interface NavItemProps {
+  to?: string;
+  children: ReactNode;
+  onClick?: () => void;
+}
+
+function NavItem({children, to, onClick}: NavItemProps) {
+  const location = useLocation();
+  const active = location?.pathname?.startsWith(to);
+
+  return (
+    <Flex
+      as={Link}
+      to={to}
+      onClick={onClick}
+      p={2}
+      fontWeight={active ? 'bold' : 'normal'}
+      align="center"
+      justify="center"
+      _hover={{bg: 'gray.100'}}
+    >
+      {children}
+    </Flex>
   );
 }
