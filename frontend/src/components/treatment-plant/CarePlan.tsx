2d8a839320a1dc862e9a999455f7d95df4626c21
Szalay Szabolcs <szotyi990204@gmail.com>
2021-10-23 22:51:23 +0200

    Add summary, edit CarePlan, add TODOs

--- frontend/src/components/treatment-plant/CarePlan.tsx
+++ frontend/src/components/treatment-plant/CarePlan.tsx
@@ -1,3 +1,4 @@
+import {GridItem, ListItem, OrderedList, UnorderedList} from '@chakra-ui/layout';
 import {ReactElement} from 'react';
 import UniformGrid from '../UniformGrid';
 import {GridContent, GridField} from './Fields';
@@ -23,6 +24,7 @@ export default function CarePlan({}: Props): ReactElement {
         When/How often
       </GridContent>
 
+      {/* TODO FOR BENDE */}
       <GridField name="ph" colSpan={6}></GridField>
       <GridField name="ph" colSpan={9}></GridField>
       <GridField name="ph" colSpan={6}></GridField>
@@ -48,6 +50,7 @@ export default function CarePlan({}: Props): ReactElement {
         How Often
       </GridContent>
 
+      {/* TODO FOR BENDE */}
       <GridField name="ph" colSpan={5}></GridField>
       <GridField name="ph" colSpan={5}></GridField>
       <GridField name="ph" colSpan={5}></GridField>
@@ -68,8 +71,43 @@ export default function CarePlan({}: Props): ReactElement {
       <GridField name="ph" colSpan={5}></GridField>
       <GridField name="ph" colSpan={5}></GridField>
 
-      <GridContent fontSize="x1" colSpan={15}>
-        TODO Bende csinád meeeg
+      {/*itt is valami TODO i guess*/}
+      <GridContent fontSize="x1" colSpan={15} textAlign="left">
+        <UniformGrid columns={1} gap={2}>
+          Please continue ro see your primary care provider for all general heath cal recommended for a man/woman your
+          age, including cancer screening tests. Any symptoms should be brought to the attention of your provider:
+          <OrderedList>
+            <ListItem>Anything that represents a brand new symptom;</ListItem>
+            <ListItem>Anything that represents a persistent symptom;</ListItem>
+            <ListItem>Anything you are worried about that might be related to the cancer coming back.</ListItem>
+          </OrderedList>
+        </UniformGrid>
+      </GridContent>
+
+      <GridContent fontSize="x1" colSpan={15} textAlign="left">
+        <UniformGrid columns={2} gap={2}>
+          <GridItem colSpan={15}>
+            Possible late- and long-term effects that someone with this type of cancer and treatment may experience:
+          </GridItem>
+          <GridItem>
+            <UnorderedList>
+              <ListItem>Constipation</ListItem>
+              <ListItem>Fatigue</ListItem>
+              <ListItem>Hair loss</ListItem>
+              <ListItem>Hearing loss</ListItem>
+              <ListItem>Kidney problems</ListItem>
+            </UnorderedList>
+          </GridItem>
+          <GridItem>
+            <UnorderedList>
+              <ListItem>Memory loss</ListItem>
+              <ListItem>Nausea</ListItem>
+              <ListItem>Peripheral neuropathy (numbness/tingling)</ListItem>
+              <ListItem>Pneumonitis or inflammation of lung tissues</ListItem>
+              <ListItem>Skin rash</ListItem>
+            </UnorderedList>
+          </GridItem>
+        </UniformGrid>
       </GridContent>
     </UniformGrid>
   );
--- frontend/src/components/treatment-plant/Summary.tsx
+++ frontend/src/components/treatment-plant/Summary.tsx
@@ -1,7 +1,95 @@
 import React, {ReactElement} from 'react';
+import UniformGrid from '../UniformGrid';
+import {GridContent, GridField, GridFieldCompact} from './Fields';
 
 interface Props {}
 
 export default function Summary({}: Props): ReactElement {
-  return <div></div>;
+  return (
+    <UniformGrid columns={6} gap={0}>
+      <GridContent color="primary.400" fontWeight="bold" fontSize="xl" colSpan={6}>
+        Treatment Summary
+      </GridContent>
+      <GridContent fontWeight="bold" fontSize="xl" colSpan={6}>
+        Diagnosis
+      </GridContent>
+
+      <GridFieldCompact colSpan={4} name="cancerType" title="Cancer Type/Location/Histology Subtype:" />
+      <GridFieldCompact colSpan={2} name="diagnosysDate" title="Diagnosis Date (year):" />
+      <GridField colSpan={6} name="stage" title="Stage Limited Stage:" />
+
+      <GridContent fontWeight="bold" fontSize="xl" colSpan={6}>
+        Treatment Completed
+      </GridContent>
+
+      <GridFieldCompact colSpan={3} name="surgery" title="Surgery TODO Checkbox" />
+      <GridFieldCompact colSpan={3} name="surgeryDate" title="Surgery Date(s) (year):" />
+      <GridFieldCompact colSpan={6} name="surgicalProcedure" title="Surgical procedure/location/findings:" />
+
+      <GridFieldCompact colSpan={2} name="radiation" title="Radiation TODO Checkbox" />
+      <GridFieldCompact colSpan={2} name="bodyAreaTreated" title="Body area treated:" />
+      <GridFieldCompact colSpan={2} name="endYear" title="End Date (year):" />
+
+      <GridFieldCompact
+        colSpan={6}
+        name="systemicTherapy"
+        title="System Therapy (chemotherapy, hormonal therapy, other): TODO Checkbox"
+      />
+
+      <GridContent fontSize="xl" colSpan={4}>
+        Names of Agents Used
+      </GridContent>
+      <GridContent fontSize="xl" colSpan={2}>
+        End Dates (year)
+      </GridContent>
+
+      <GridFieldCompact colSpan={4} name="agentsUsed" title="TODO checkbox Carboplatin" />
+      <GridFieldCompact colSpan={2} name="" title="" />
+      <GridFieldCompact colSpan={4} name="agentsUsed" title="TODO checkbox Cisplatin" />
+      <GridFieldCompact colSpan={2} name="" title="" />
+      <GridFieldCompact colSpan={4} name="agentsUsed" title="TODO checkbox Etoposide" />
+      <GridFieldCompact colSpan={2} name="" title="" />
+      <GridFieldCompact colSpan={4} name="agentsUsed" title="TODO checkbox Paclitaxel" />
+      <GridFieldCompact colSpan={2} name="" title="" />
+      <GridFieldCompact colSpan={4} name="agentsUsed" title="TODO checkbox Vincristine" />
+      <GridFieldCompact colSpan={2} name="" title="" />
+      <GridFieldCompact colSpan={4} name="agentsUsed" title="TODO checkbox Other" />
+      <GridFieldCompact colSpan={2} name="" title="" />
+
+      <GridField
+        colSpan={6}
+        name="sympthomsSideEffects"
+        title="Persistent symptoms os side effects of treatment: TODO checkbox (enter type(s)):"
+      />
+      {/* TODO box for name="symptomsSideEffectsDescription" */}
+
+      <GridContent fontWeight="bold" fontSize="xl" colSpan={6}>
+        Treatment Ongoing
+      </GridContent>
+
+      <GridFieldCompact
+        colSpan={6}
+        name="needOngoingTreatment"
+        title="Need for ongoing (adjuvant) treatment for cancer: TODO Checkbox"
+      />
+
+      <GridContent fontSize="xl" colSpan={2}>
+        Additional treatment name
+      </GridContent>
+      <GridContent fontSize="xl" colSpan={2}>
+        Planned duration
+      </GridContent>
+      <GridContent fontSize="xl" colSpan={2}>
+        Posibble Side effects
+      </GridContent>
+
+      {/*TODO fillek */}
+      <GridFieldCompact colSpan={2} name="" title="" />
+      <GridFieldCompact colSpan={2} name="" title="" />
+      <GridFieldCompact colSpan={2} name="" title="" />
+      <GridFieldCompact colSpan={2} name="" title="" />
+      <GridFieldCompact colSpan={2} name="" title="" />
+      <GridFieldCompact colSpan={2} name="" title="" />
+    </UniformGrid>
+  );
 }
