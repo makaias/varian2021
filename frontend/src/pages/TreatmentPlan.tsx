cce8f2b1d24cd2913fdc1efa423ab9d25f180795
Bendegúz Cseresnyés <cser.bende@gmail.com>
2021-10-24 01:59:34 +0200

    Finalize Treatment plan

--- frontend/src/pages/TreatmentPlan.tsx
+++ frontend/src/pages/TreatmentPlan.tsx
@@ -1,11 +1,18 @@
+import {Box} from '@chakra-ui/layout';
 import React, {ReactElement} from 'react';
 import {useParams} from 'react-router';
+import {useLayoutConfig} from '../app/layout';
 import TreatmentPlanDocument from '../components/treatment-plant/TreatmentPlanDocument';
 
 interface Props {}
 
 export default function TreatmentPlan({}: Props): ReactElement {
+  useLayoutConfig({title: 'Treatment summary', bg: 'plain'});
   const {id} = useParams<{id}>();
 
-  return <TreatmentPlanDocument id={id} />;
+  return (
+    <Box py={4}>
+      <TreatmentPlanDocument id={id} />;
+    </Box>
+  );
 }
