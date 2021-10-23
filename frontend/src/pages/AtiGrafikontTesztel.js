92ac58301444a4934a95e8701c0bc2199307dd92
Bendegúz Cseresnyés <cser.bende@gmail.com>
2021-10-23 03:42:41 +0200

    Fix responsive diagrams

--- frontend/src/pages/AtiGrafikontTesztel.js
+++ frontend/src/pages/AtiGrafikontTesztel.js
@@ -1,44 +1,10 @@
-import React, {ReactElement} from 'react';
-import useEndpoint from '../hooks/useEndpoint';
-import {Line} from 'react-chartjs-2';
+import React from 'react';
+import LineDiagram from '../components/diagrams/LineDiagram';
 import UniformGrid from '../components/UniformGrid';
-
-function DiagramCreator({data, title}) {
-  const state = {
-    labels: [...data.keys()],
-    datasets: [
-      {
-        label: title,
-        fill: false,
-        lineTension: 0.5,
-        backgroundColor: 'rgba(75,192,192,1)',
-        borderColor: 'rgba(0,0,0,1)',
-        borderWidth: 2,
-        data: data.map((item) => item.value),
-      },
-    ],
-  };
-  return (
-    <Line
-      data={state}
-      options={{
-        responsive: true,
-        title: {
-          display: true,
-          text: title,
-          fontSize: 20,
-        },
-        legend: {
-          display: true,
-          position: 'right',
-        },
-      }}
-    />
-  );
-}
+import useEndpoint from '../hooks/useEndpoint';
 
 export default function AtiGrafikontTesztel({}) {
-  const patientId = 7;
+  const patientId = 2;
   const usedEndpoint = useEndpoint({
     conf: {
       url: `/statistics/${patientId}`,
@@ -52,10 +18,14 @@ export default function AtiGrafikontTesztel({}) {
       {usedEndpoint.failed && <p>Failed</p>}
       {usedEndpoint.succeeded && (
         <UniformGrid columns={[1, 1, 2]} gap={4}>
-          <DiagramCreator data={usedEndpoint.data.lifeStatisfaction} title="Life statisfaction" />
-          <DiagramCreator data={usedEndpoint.data.eatingBehavior} title="Eating behavior" />
+          <LineDiagram
+            data={usedEndpoint.data.lifeStatisfaction.map((i) => i.value)}
+            labels={[...usedEndpoint.data.lifeStatisfaction.keys()]}
+            title="Life statisfaction"
+          />
+          {/*<DiagramCreator data={usedEndpoint.data.eatingBehavior} title="Eating behavior" />
           <DiagramCreator data={usedEndpoint.data.sleeping} title="Sleeping" />
-          <DiagramCreator data={usedEndpoint.data.socialInteraction} title="Social interaction" />
+      <DiagramCreator data={usedEndpoint.data.socialInteraction} title="Social interaction" />*/}
         </UniformGrid>
       )}
     </>
