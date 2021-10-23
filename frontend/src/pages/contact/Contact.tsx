28e076a125802449c64d9147571a2f5816db3d67
Dzsani <gatsjanos@gmail.com>
2021-10-23 05:36:54 +0200

    contact page design

--- frontend/src/pages/contact/Contact.tsx
+++ frontend/src/pages/contact/Contact.tsx
@@ -7,8 +7,7 @@ import UniformGrid from '../../components/UniformGrid';
 
 export default function Contact(): ReactElement {
   return (
-    <Container pt={8} maxWidth='container.sm'>
-
+    <>
       <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
         dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
         commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
@@ -44,6 +43,6 @@ export default function Contact(): ReactElement {
 
         </VStack>
       </UniformGrid>
-    </Container>
+    </>
   );
 };
