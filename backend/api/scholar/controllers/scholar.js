ad350e3b240c5d80e30840bbc81ed76fd7e594ea
Dzsani <gatsjanos@gmail.com>
2021-10-24 06:46:01 +0200

    Scholar improvements

--- backend/api/scholar/controllers/scholar.js
+++ backend/api/scholar/controllers/scholar.js
@@ -1,7 +1,10 @@
 const axios = require("axios");
 
-const apiKey =
-  "e46c7289f24ab432fff79125001322df5805788047915a733f21572c1f783e4f";
+//comment out new addresses if you want more 100 serches from a new serpapi account
+const apiKey = 'e46c7289f24ab432fff79125001322df5805788047915a733f21572c1f783e4f';
+// const apiKey = '857b1e8a45957acbb57da7b9467d9b58d9d0b7b7ff975083f71c3b3f305536fd';
+// const apiKey = 'd44456390869dcf3c0d1920b441318563f504b2d1857f30919fb7ef984eae1bd';
+// const apiKey = '84378c066ca6c946cb7ca016ceced5854711a3b31c266fc2f598ea4b123ca2d0';
 
 module.exports = {
   async fetch(ctx) {
--- frontend/src/components/scholar/ScholarBlock.tsx
+++ frontend/src/components/scholar/ScholarBlock.tsx
@@ -15,9 +15,9 @@ export default function ScholarBlock({defaultKeyword: defaultKeyword, itemCount}
 
   const usedEndpoint = useEndpoint({
     conf: {
-      url: `/scholar/${keyword}?num=${itemCount}`,
+      url: `/scholar/${keyword}?num=${itemCount}`
     },
-    deps: [itemCount],
+    deps: [itemCount]
   });
 
   const results = usedEndpoint.data;
@@ -25,41 +25,47 @@ export default function ScholarBlock({defaultKeyword: defaultKeyword, itemCount}
   return (
     <>
       {usedEndpoint.pending && (
-        <Flex justify="center">
+        <Flex justify='center'>
           <Spinner></Spinner>
         </Flex>
       )}
       {usedEndpoint.failed && <p>Failed</p>}
       {usedEndpoint.succeeded && (
         <>
-          <HStack justify="center">
-            <InputGroup width="md" margin="1rem">
+          <HStack justify='center'>
+            <InputGroup width='md' margin='1rem'>
               <Input
+                autoFocus={true}
                 value={keyword}
                 onChange={(e) => setKeyword(e.target.value)}
-                backgroundColor="white"
-                variant="outline"
-                placeholder="Search..."
-                borderColor="primary.500"
-                color="primary.500"
+                onKeyPress={e => {
+                  if (e.key === 'Enter') {
+                    handleClick();
+                  }
+                }}
+                backgroundColor='white'
+                variant='outline'
+                placeholder='Search...'
+                borderColor='primary.500'
+                color='primary.500'
               />
-              <InputRightElement width="2rem">
-                <Button backgroundColor="primary.500" color="white" size="md" onClick={handleClick}>
+              <InputRightElement width='2rem'>
+                <Button backgroundColor='primary.500' color='white' size='md' onClick={handleClick}>
                   GO
                 </Button>
               </InputRightElement>
             </InputGroup>
           </HStack>
-          {results['organic_results'].map((x, index) => (
+          {results?.['organic_results']?.map((x, index) => (
             <>
-              <Box key={index} margin="1rem" padding="1rem" borderTop="1px" borderTopColor="primary.500">
+              <Box key={index} margin='1rem' padding='1rem' borderTop='1px' borderTopColor='primary.500'>
                 <LinkBox>
-                  <Text fontSize="xl" fontWeight="bold" color="primary.500">
+                  <Text fontSize='xl' fontWeight='bold' color='primary.500'>
                     {x.title}
                   </Text>
                   <Text>{x.snippet}</Text>
-                  <Text fontStyle="italic">{x.publication_info?.summary}</Text>
-                  <LinkOverlay href={x.link} target="_blank"></LinkOverlay>
+                  <Text fontStyle='italic'>{x.publication_info?.summary}</Text>
+                  <LinkOverlay href={x.link} target='_blank'></LinkOverlay>
                 </LinkBox>
               </Box>
             </>
